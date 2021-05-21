b79436ed5655dcae6536871de84498c0db81148a
Ariel Jakubowski <arieljakubowski@gmail.com>
2021-05-20 20:14:40 -0400

    Planning

--- Planning.js
+++ Planning.js
@@ -1,2 +1,13 @@
 // Users can select a fauxkemon to use
+// Users can choose a fauxkemon to battle or choose a random fauxkemon
+
+
+
+
+// fauxkemon
+// name
+// type
+// HP 
+// Attacks (3 each?)
+// image
 // 
